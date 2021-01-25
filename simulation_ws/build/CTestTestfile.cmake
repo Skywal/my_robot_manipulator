5437e381820b460b706d26c95c1e0306715f67ee
Andrew Derkach Bloom <skaywoker6@gmail.com>
2021-01-25 22:50:40 +0200

    Initial project with two workspaces and make rviz simulation

---
+++ simulation_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/zero/Documents/my_robot_manipulator/simulation_ws/src
+# Build directory: /home/zero/Documents/my_robot_manipulator/simulation_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("mrm_description")
